e3c66e13a086de6dda6e73905bfed684acc65766
twDevNoob <smelowsergey@gmail.com>
2022-11-06 02:18:46 +0600

    Add switching between meanings

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const input = document.querySelector(".search-area__input")
 const searchButton = document.querySelector(".search-area__button")
 const outputSection = document.querySelector(".output")
+const otherMeaningsButton = document.querySelector(".output__other-meanings")
 
 async function displayData() {
 	const url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 	const wordTitle = document.querySelector(".word__title")
 	const word = input.value
+	let counter = 0
 
 	try {
 		const response = await fetch(`${url}${word}`)
 		const data = await response.json()
 		console.log(data)
 
+		function assignValues(index) {
+			document.querySelector(".details__part-of-speech").textContent = data[0].meanings[index].partOfSpeech
+			document.querySelector(".output__meaning").textContent = data[0].meanings[index].definitions[0].definition
+			document.querySelector(".output__example").textContent =
+				data[0].meanings[index].definitions[0].example || ""
+		}
+
 		wordTitle.textContent = word
 
-		document.querySelector(".details__part-of-speech").textContent = data[0].meanings[0].partOfSpeech
 		document.querySelector(".details__transcription").textContent = data[0].phonetic
-		document.querySelector(".output__meaning").textContent = data[0].meanings[0].definitions[0].definition
-		console.log(data[0].meanings[0].definitions)
-		document.querySelector(".output__example").textContent = data[0].meanings[0].definitions[0].example || ""
+		assignValues(0)
+
+		otherMeaningsButton.addEventListener("click", () => {
+			counter = counter === data[0].meanings.length - 1 ? 0 : counter + 1
+			assignValues(counter)
+		})
 	} catch (error) {
 		wordTitle.textContent = "Could not find the word"
 		console.log(error)
