fbf5465aca1878a567f28f000c9b38447fc68833
twDevNoob <smelowsergey@gmail.com>
2022-11-05 22:52:58 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const input = document.querySelector(".search-area__input")
+const searchButton = document.querySelector(".search-area__button")
+const outputSection = document.querySelector(".output")
+
+searchButton.addEventListener("click", async function () {
+	const url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+
+	const wordTitle = document.querySelector(".word__title")
+	const partOfSpeech = document.querySelector(".details__part-of-speech")
+	const transcription = document.querySelector(".details__transcription")
+	const meaning = document.querySelector(".output__meaning")
+	const example = document.querySelector(".output__example")
+
+	const word = input.value
+
+	try {
+		const response = await fetch(`${url}${word}`)
+		const data = await response.json()
+		console.log(data)
+
+		wordTitle.textContent = word
+
+		partOfSpeech.textContent = data[0].meanings[0].partOfSpeech
+		transcription.textContent = data[0].phonetic
+		meaning.textContent = data[0].meanings[0].definitions[0].definition
+		example.textContent = data[0].meanings[0].definitions[0].example || ""
+	} catch (error) {
+		wordTitle.textContent = "Could not find the word"
+		console.log(error)
+	}
+
+	outputSection.classList.remove("output--hidden")
+})
